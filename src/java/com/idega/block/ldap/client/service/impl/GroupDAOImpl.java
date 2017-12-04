c61833c9613e7ae7eea96d58a43f14d26beae8d4
Valdas Žemaitis <valdas@idega.com>
2017-12-04 10:17:10 +0200

    Checking if group is known

--- src/java/com/idega/block/ldap/client/service/impl/GroupDAOImpl.java
+++ src/java/com/idega/block/ldap/client/service/impl/GroupDAOImpl.java
@@ -3,82 +3,82 @@
  *
  * Idega Software hf. Source Code Licence Agreement x
  *
- * This agreement, made this 10th of February 2006 by and between 
- * Idega Software hf., a business formed and operating under laws 
- * of Iceland, having its principal place of business in Reykjavik, 
- * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * This agreement, made this 10th of February 2006 by and between
+ * Idega Software hf., a business formed and operating under laws
+ * of Iceland, having its principal place of business in Reykjavik,
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura
  * IT hereinafter referred to as "Licensee".
- * 1.  License Grant: Upon completion of this agreement, the source 
- *     code that may be made available according to the documentation for 
- *     a particular software product (Software) from Manufacturer 
- *     (Source Code) shall be provided to Licensee, provided that 
- *     (1) funds have been received for payment of the License for Software and 
- *     (2) the appropriate License has been purchased as stated in the 
- *     documentation for Software. As used in this License Agreement, 
- *     Licensee shall also mean the individual using or installing 
- *     the source code together with any individual or entity, including 
- *     but not limited to your employer, on whose behalf you are acting 
- *     in using or installing the Source Code. By completing this agreement, 
- *     Licensee agrees to be bound by the terms and conditions of this Source 
- *     Code License Agreement. This Source Code License Agreement shall 
- *     be an extension of the Software License Agreement for the associated 
- *     product. No additional amendment or modification shall be made 
- *     to this Agreement except in writing signed by Licensee and 
+ * 1.  License Grant: Upon completion of this agreement, the source
+ *     code that may be made available according to the documentation for
+ *     a particular software product (Software) from Manufacturer
+ *     (Source Code) shall be provided to Licensee, provided that
+ *     (1) funds have been received for payment of the License for Software and
+ *     (2) the appropriate License has been purchased as stated in the
+ *     documentation for Software. As used in this License Agreement,
+ *     Licensee shall also mean the individual using or installing
+ *     the source code together with any individual or entity, including
+ *     but not limited to your employer, on whose behalf you are acting
+ *     in using or installing the Source Code. By completing this agreement,
+ *     Licensee agrees to be bound by the terms and conditions of this Source
+ *     Code License Agreement. This Source Code License Agreement shall
+ *     be an extension of the Software License Agreement for the associated
+ *     product. No additional amendment or modification shall be made
+ *     to this Agreement except in writing signed by Licensee and
  *     Manufacturer. This Agreement is effective indefinitely and once
- *     completed, cannot be terminated. Manufacturer hereby grants to 
- *     Licensee a non-transferable, worldwide license during the term of 
- *     this Agreement to use the Source Code for the associated product 
- *     purchased. In the event the Software License Agreement to the 
- *     associated product is terminated; (1) Licensee's rights to use 
- *     the Source Code are revoked and (2) Licensee shall destroy all 
- *     copies of the Source Code including any Source Code used in 
+ *     completed, cannot be terminated. Manufacturer hereby grants to
+ *     Licensee a non-transferable, worldwide license during the term of
+ *     this Agreement to use the Source Code for the associated product
+ *     purchased. In the event the Software License Agreement to the
+ *     associated product is terminated; (1) Licensee's rights to use
+ *     the Source Code are revoked and (2) Licensee shall destroy all
+ *     copies of the Source Code including any Source Code used in
  *     Licensee's applications.
  * 2.  License Limitations
- *     2.1 Licensee may not resell, rent, lease or distribute the 
- *         Source Code alone, it shall only be distributed as a 
+ *     2.1 Licensee may not resell, rent, lease or distribute the
+ *         Source Code alone, it shall only be distributed as a
  *         compiled component of an application.
- *     2.2 Licensee shall protect and keep secure all Source Code 
- *         provided by this this Source Code License Agreement. 
- *         All Source Code provided by this Agreement that is used 
+ *     2.2 Licensee shall protect and keep secure all Source Code
+ *         provided by this this Source Code License Agreement.
+ *         All Source Code provided by this Agreement that is used
  *         with an application that is distributed or accessible outside
- *         Licensee's organization (including use from the Internet), 
- *         must be protected to the extent that it cannot be easily 
+ *         Licensee's organization (including use from the Internet),
+ *         must be protected to the extent that it cannot be easily
  *         extracted or decompiled.
- *     2.3 The Licensee shall not resell, rent, lease or distribute 
- *         the products created from the Source Code in any way that 
+ *     2.3 The Licensee shall not resell, rent, lease or distribute
+ *         the products created from the Source Code in any way that
  *         would compete with Idega Software.
- *     2.4 Manufacturer's copyright notices may not be removed from 
+ *     2.4 Manufacturer's copyright notices may not be removed from
  *         the Source Code.
- *     2.5 All modifications on the source code by Licencee must 
+ *     2.5 All modifications on the source code by Licencee must
  *         be submitted to or provided to Manufacturer.
- * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
- *     proprietary information. Licensee shall not distribute or 
- *     reveal the Source Code to anyone other than the software 
- *     developers of Licensee's organization. Licensee may be held 
- *     legally responsible for any infringement of intellectual property 
- *     rights that is caused or encouraged by Licensee's failure to abide 
- *     by the terms of this Agreement. Licensee may make copies of the 
- *     Source Code provided the copyright and trademark notices are 
- *     reproduced in their entirety on the copy. Manufacturer reserves 
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains
+ *     proprietary information. Licensee shall not distribute or
+ *     reveal the Source Code to anyone other than the software
+ *     developers of Licensee's organization. Licensee may be held
+ *     legally responsible for any infringement of intellectual property
+ *     rights that is caused or encouraged by Licensee's failure to abide
+ *     by the terms of this Agreement. Licensee may make copies of the
+ *     Source Code provided the copyright and trademark notices are
+ *     reproduced in their entirety on the copy. Manufacturer reserves
  *     all rights not specifically granted to Licensee.
  *
- * 4.  Warranty & Risks: Although efforts have been made to assure that the 
- *     Source Code is correct, reliable, date compliant, and technically 
- *     accurate, the Source Code is licensed to Licensee as is and without 
- *     warranties as to performance of merchantability, fitness for a 
- *     particular purpose or use, or any other warranties whether 
- *     expressed or implied. Licensee's organization and all users 
- *     of the source code assume all risks when using it. The manufacturers, 
- *     distributors and resellers of the Source Code shall not be liable 
- *     for any consequential, incidental, punitive or special damages 
- *     arising out of the use of or inability to use the source code or 
- *     the provision of or failure to provide support services, even if we 
- *     have been advised of the possibility of such damages. In any case, 
- *     the entire liability under any provision of this agreement shall be 
- *     limited to the greater of the amount actually paid by Licensee for the 
- *     Software or 5.00 USD. No returns will be provided for the associated 
- *     License that was purchased to become eligible to receive the Source 
- *     Code after Licensee receives the source code. 
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the
+ *     Source Code is correct, reliable, date compliant, and technically
+ *     accurate, the Source Code is licensed to Licensee as is and without
+ *     warranties as to performance of merchantability, fitness for a
+ *     particular purpose or use, or any other warranties whether
+ *     expressed or implied. Licensee's organization and all users
+ *     of the source code assume all risks when using it. The manufacturers,
+ *     distributors and resellers of the Source Code shall not be liable
+ *     for any consequential, incidental, punitive or special damages
+ *     arising out of the use of or inability to use the source code or
+ *     the provision of or failure to provide support services, even if we
+ *     have been advised of the possibility of such damages. In any case,
+ *     the entire liability under any provision of this agreement shall be
+ *     limited to the greater of the amount actually paid by Licensee for the
+ *     Software or 5.00 USD. No returns will be provided for the associated
+ *     License that was purchased to become eligible to receive the Source
+ *     Code after Licensee receives the source code.
  */
 package com.idega.block.ldap.client.service.impl;
 
@@ -182,7 +182,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param group to get distinguished names tree for, not <code>null</code>
 	 * @return {@link Map} of distinguished name part and {@link Group} of that name or {@link Collections#emptyMap()}
 	 * on failure;
@@ -200,7 +200,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 							/*
 							 * Ensuring that path is appended only to direct parent group
 							 */
-							
+
 
 							StringBuilder path = new StringBuilder(OrganizationalUnit.ORGANIZATIONAL_UNIT_NAME).append(CoreConstants.EQ).append(getGroupName(group));
 							path.append(CoreConstants.COMMA);
@@ -224,7 +224,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param group to get tree of {@link DN}s fo, not <code>null</code>
 	 * @return distinguished name objects or {@link Collections#emptyMap()} on failure
 	 * @throws LDAPException if the provided string cannot be parsed as a valid DN.
@@ -249,12 +249,12 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param distinguishedName to fetch results by, not <code>null</code>
 	 * @return entities by {@link DN} or {@link Collections#emptyList()} on failure
-	 * @throws LDAPSearchException if the search does not complete successfully, or if a problem is encountered while 
-	 * sending the request or reading the response. If one or more entries or references were returned before the 
-	 * failure was encountered, then the LDAPSearchException object may be examined to obtain information about those 
+	 * @throws LDAPSearchException if the search does not complete successfully, or if a problem is encountered while
+	 * sending the request or reading the response. If one or more entries or references were returned before the
+	 * failure was encountered, then the LDAPSearchException object may be examined to obtain information about those
 	 * entries and/or references.
 	 * @throws GeneralSecurityException there are problems with TLS/SSL connection.
 	 * @throws LDAPException if a problem occurs while attempting to connect to the specified server.
@@ -271,7 +271,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 					ejbGroup = getGroupHome().findGroupByUniqueId(entry.getAttributeValue(OrganizationalUnit.DESCRIPTION));
 				} catch (FinderException e) {}
 
-				Group entity = getGroupDAO().findGroup((Integer) ejbGroup.getPrimaryKey());
+				Group entity = ejbGroup == null ? null : getGroupDAO().findGroup((Integer) ejbGroup.getPrimaryKey());
 				if (entity != null) {
 					entities.add(entity);
 				}
@@ -307,9 +307,9 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 			} catch (LDAPSearchException e) {}
 
 			if (existingEntities == null) {
-				
+
 				String timeout = getApplicationProperty(
-						ConnectionService.PROPERTY_RESPONSE_TIMEOUT, 
+						ConnectionService.PROPERTY_RESPONSE_TIMEOUT,
 						ConnectionService.DEFAULT_RESPONSE_TIMEOUT.toString());
 
 				AddRequest request = new AddRequest(new DN(dn));
@@ -333,7 +333,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 	}
 
 	/**
-	 * 
+	 *
 	 * <p>Creates new entity</p>
 	 * @param distinguishedName of entity, not <code>null</code>
 	 * @param entity itself to write, not <code>null</code>
@@ -344,7 +344,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 	private Group create(DN distinguishedName, Group entity) throws LDAPException, GeneralSecurityException {
 		if (entity != null) {
 			String timeout = getApplicationProperty(
-					ConnectionService.PROPERTY_RESPONSE_TIMEOUT, 
+					ConnectionService.PROPERTY_RESPONSE_TIMEOUT,
 					ConnectionService.DEFAULT_RESPONSE_TIMEOUT.toString());
 
 			AddRequest request = new AddRequest(distinguishedName);
@@ -404,23 +404,23 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 			String groupName = getGroupName(entity);
 			if (!StringUtil.isEmpty(groupName)) {
 				modifications.add(new Modification(
-						ModificationType.REPLACE, 
-						OrganizationalUnit.ORGANIZATIONAL_UNIT_NAME, 
+						ModificationType.REPLACE,
+						OrganizationalUnit.ORGANIZATIONAL_UNIT_NAME,
 						groupName));
 			}
 
 			if (!StringUtil.isEmpty(entity.getUniqueId())) {
 				modifications.add(new Modification(
-						ModificationType.REPLACE, 
-						OrganizationalUnit.DESCRIPTION, 
+						ModificationType.REPLACE,
+						OrganizationalUnit.DESCRIPTION,
 						entity.getUniqueId()));
 			}
 
 			GroupType type = entity.getGroupType();
 			if (type != null && !StringUtil.isEmpty(type.getGroupType())) {
 				modifications.add(new Modification(
-						ModificationType.REPLACE, 
-						OrganizationalUnit.BUSINESS_CATEGORY, 
+						ModificationType.REPLACE,
+						OrganizationalUnit.BUSINESS_CATEGORY,
 						type.getGroupType()));
 			}
 
@@ -444,7 +444,7 @@ public class GroupDAOImpl extends DefaultSpringBean implements GroupDAO {
 	@Override
 	public List<Group> update(Group group) throws LDAPException, GeneralSecurityException {
 		ArrayList<Group> updatedGroups = new ArrayList<>();
-		
+
 		TreeMap<DN, Group> distinguishedNames = getDistinguishedNames(group);
 		for (DN distinguishedName : distinguishedNames.keySet()) {
 			Group updatedGroup = update(distinguishedName, distinguishedNames.get(distinguishedName));
