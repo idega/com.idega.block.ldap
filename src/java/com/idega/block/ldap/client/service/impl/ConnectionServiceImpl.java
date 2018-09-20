e4a891c03513be72eca1bcea2166bc90088a67b2
Valdas Žemaitis <valdas@idega.com>
2018-09-20 13:56:27 +0200

    Using constant

--- src/java/com/idega/block/ldap/client/service/impl/ConnectionServiceImpl.java
+++ src/java/com/idega/block/ldap/client/service/impl/ConnectionServiceImpl.java
@@ -3,86 +3,85 @@
  *
  * Idega Software hf. Source Code Licence Agreement x
  *
- * This agreement, made this 10th of February 2006 by and between 
- * Idega Software hf., a business formed and operating under laws 
- * of Iceland, having its principal place of business in Reykjavik, 
- * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * This agreement, made this 10th of February 2006 by and between
+ * Idega Software hf., a business formed and operating under laws
+ * of Iceland, having its principal place of business in Reykjavik,
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura
  * IT hereinafter referred to as "Licensee".
- * 1.  License Grant: Upon completion of this agreement, the source 
- *     code that may be made available according to the documentation for 
- *     a particular software product (Software) from Manufacturer 
- *     (Source Code) shall be provided to Licensee, provided that 
- *     (1) funds have been received for payment of the License for Software and 
- *     (2) the appropriate License has been purchased as stated in the 
- *     documentation for Software. As used in this License Agreement, 
- *     Licensee shall also mean the individual using or installing 
- *     the source code together with any individual or entity, including 
- *     but not limited to your employer, on whose behalf you are acting 
- *     in using or installing the Source Code. By completing this agreement, 
- *     Licensee agrees to be bound by the terms and conditions of this Source 
- *     Code License Agreement. This Source Code License Agreement shall 
- *     be an extension of the Software License Agreement for the associated 
- *     product. No additional amendment or modification shall be made 
- *     to this Agreement except in writing signed by Licensee and 
+ * 1.  License Grant: Upon completion of this agreement, the source
+ *     code that may be made available according to the documentation for
+ *     a particular software product (Software) from Manufacturer
+ *     (Source Code) shall be provided to Licensee, provided that
+ *     (1) funds have been received for payment of the License for Software and
+ *     (2) the appropriate License has been purchased as stated in the
+ *     documentation for Software. As used in this License Agreement,
+ *     Licensee shall also mean the individual using or installing
+ *     the source code together with any individual or entity, including
+ *     but not limited to your employer, on whose behalf you are acting
+ *     in using or installing the Source Code. By completing this agreement,
+ *     Licensee agrees to be bound by the terms and conditions of this Source
+ *     Code License Agreement. This Source Code License Agreement shall
+ *     be an extension of the Software License Agreement for the associated
+ *     product. No additional amendment or modification shall be made
+ *     to this Agreement except in writing signed by Licensee and
  *     Manufacturer. This Agreement is effective indefinitely and once
- *     completed, cannot be terminated. Manufacturer hereby grants to 
- *     Licensee a non-transferable, worldwide license during the term of 
- *     this Agreement to use the Source Code for the associated product 
- *     purchased. In the event the Software License Agreement to the 
- *     associated product is terminated; (1) Licensee's rights to use 
- *     the Source Code are revoked and (2) Licensee shall destroy all 
- *     copies of the Source Code including any Source Code used in 
+ *     completed, cannot be terminated. Manufacturer hereby grants to
+ *     Licensee a non-transferable, worldwide license during the term of
+ *     this Agreement to use the Source Code for the associated product
+ *     purchased. In the event the Software License Agreement to the
+ *     associated product is terminated; (1) Licensee's rights to use
+ *     the Source Code are revoked and (2) Licensee shall destroy all
+ *     copies of the Source Code including any Source Code used in
  *     Licensee's applications.
  * 2.  License Limitations
- *     2.1 Licensee may not resell, rent, lease or distribute the 
- *         Source Code alone, it shall only be distributed as a 
+ *     2.1 Licensee may not resell, rent, lease or distribute the
+ *         Source Code alone, it shall only be distributed as a
  *         compiled component of an application.
- *     2.2 Licensee shall protect and keep secure all Source Code 
- *         provided by this this Source Code License Agreement. 
- *         All Source Code provided by this Agreement that is used 
+ *     2.2 Licensee shall protect and keep secure all Source Code
+ *         provided by this this Source Code License Agreement.
+ *         All Source Code provided by this Agreement that is used
  *         with an application that is distributed or accessible outside
- *         Licensee's organization (including use from the Internet), 
- *         must be protected to the extent that it cannot be easily 
+ *         Licensee's organization (including use from the Internet),
+ *         must be protected to the extent that it cannot be easily
  *         extracted or decompiled.
- *     2.3 The Licensee shall not resell, rent, lease or distribute 
- *         the products created from the Source Code in any way that 
+ *     2.3 The Licensee shall not resell, rent, lease or distribute
+ *         the products created from the Source Code in any way that
  *         would compete with Idega Software.
- *     2.4 Manufacturer's copyright notices may not be removed from 
+ *     2.4 Manufacturer's copyright notices may not be removed from
  *         the Source Code.
- *     2.5 All modifications on the source code by Licencee must 
+ *     2.5 All modifications on the source code by Licencee must
  *         be submitted to or provided to Manufacturer.
- * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
- *     proprietary information. Licensee shall not distribute or 
- *     reveal the Source Code to anyone other than the software 
- *     developers of Licensee's organization. Licensee may be held 
- *     legally responsible for any infringement of intellectual property 
- *     rights that is caused or encouraged by Licensee's failure to abide 
- *     by the terms of this Agreement. Licensee may make copies of the 
- *     Source Code provided the copyright and trademark notices are 
- *     reproduced in their entirety on the copy. Manufacturer reserves 
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains
+ *     proprietary information. Licensee shall not distribute or
+ *     reveal the Source Code to anyone other than the software
+ *     developers of Licensee's organization. Licensee may be held
+ *     legally responsible for any infringement of intellectual property
+ *     rights that is caused or encouraged by Licensee's failure to abide
+ *     by the terms of this Agreement. Licensee may make copies of the
+ *     Source Code provided the copyright and trademark notices are
+ *     reproduced in their entirety on the copy. Manufacturer reserves
  *     all rights not specifically granted to Licensee.
  *
- * 4.  Warranty & Risks: Although efforts have been made to assure that the 
- *     Source Code is correct, reliable, date compliant, and technically 
- *     accurate, the Source Code is licensed to Licensee as is and without 
- *     warranties as to performance of merchantability, fitness for a 
- *     particular purpose or use, or any other warranties whether 
- *     expressed or implied. Licensee's organization and all users 
- *     of the source code assume all risks when using it. The manufacturers, 
- *     distributors and resellers of the Source Code shall not be liable 
- *     for any consequential, incidental, punitive or special damages 
- *     arising out of the use of or inability to use the source code or 
- *     the provision of or failure to provide support services, even if we 
- *     have been advised of the possibility of such damages. In any case, 
- *     the entire liability under any provision of this agreement shall be 
- *     limited to the greater of the amount actually paid by Licensee for the 
- *     Software or 5.00 USD. No returns will be provided for the associated 
- *     License that was purchased to become eligible to receive the Source 
- *     Code after Licensee receives the source code. 
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the
+ *     Source Code is correct, reliable, date compliant, and technically
+ *     accurate, the Source Code is licensed to Licensee as is and without
+ *     warranties as to performance of merchantability, fitness for a
+ *     particular purpose or use, or any other warranties whether
+ *     expressed or implied. Licensee's organization and all users
+ *     of the source code assume all risks when using it. The manufacturers,
+ *     distributors and resellers of the Source Code shall not be liable
+ *     for any consequential, incidental, punitive or special damages
+ *     arising out of the use of or inability to use the source code or
+ *     the provision of or failure to provide support services, even if we
+ *     have been advised of the possibility of such damages. In any case,
+ *     the entire liability under any provision of this agreement shall be
+ *     limited to the greater of the amount actually paid by Licensee for the
+ *     Software or 5.00 USD. No returns will be provided for the associated
+ *     License that was purchased to become eligible to receive the Source
+ *     Code after Licensee receives the source code.
  */
 package com.idega.block.ldap.client.service.impl;
 
-import java.net.Socket;
 import java.security.GeneralSecurityException;
 
 import javax.net.SocketFactory;
@@ -139,7 +138,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 		if (this.trustManager == null) {
 			String domain = getApplicationProperty(PROPERTY_DOMAIN, DEFAULT_DOMAIN);
 
-			this.trustManager = new AggregateTrustManager(Boolean.TRUE, 
+			this.trustManager = new AggregateTrustManager(Boolean.TRUE,
 					new HostNameTrustManager(Boolean.FALSE, domain),
 					new ValidityDateTrustManager());
 		}
@@ -150,7 +149,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 	private LDAPConnectionOptions getConnectionOptions() {
 		if (this.connectionOptions == null) {
 			String timeout = getApplicationProperty(PROPERTY_TIMEOUT, DEFAULT_TIMEOUT.toString());
-			
+
 			this.connectionOptions = new LDAPConnectionOptions();
 			this.connectionOptions.setAbandonOnTimeout(Boolean.TRUE);
 			this.connectionOptions.setConnectTimeoutMillis(Integer.valueOf(timeout));
@@ -169,7 +168,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 
 	private SocketFactory getSocketFactory() throws GeneralSecurityException {
 		if (this.socketFactory == null) {
-		    this.socketFactory = SocketFactory.getDefault(); 
+		    this.socketFactory = SocketFactory.getDefault();
 		}
 
 		return this.socketFactory;
@@ -205,7 +204,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 		SearchRequest request = new SearchRequest(distinguishedName.toString(), SearchScope.SUB, filter);
 		request.setSizeLimit(Integer.valueOf(size));
 		request.setTimeLimitSeconds(Integer.valueOf(timeout));
-		
+
 		LDAPConnection connection = getConnection();
 		SearchResult response = connection.search(request);
 		connection.close();
@@ -221,7 +220,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 	public SearchResult findByDN(Filter filter, String commaSeparatedDN) throws LDAPSearchException, LDAPException, GeneralSecurityException {
 		String baseDN = getApplicationProperty(PROPERTY_DOMAIN_DN, DEFAULT_DOMAIN_DN);
 		if (!StringUtil.isEmpty(baseDN)) {
-			return findByDN(filter, new DN(commaSeparatedDN + "," + baseDN));
+			return findByDN(filter, new DN(commaSeparatedDN + CoreConstants.COMMA + baseDN));
 		}
 
 		return null;
@@ -234,7 +233,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 	@Override
 	public SearchResult findByUUID(Filter filter, String organizationalUnit, String uuid) throws LDAPSearchException, LDAPException, GeneralSecurityException {
 		if (!StringUtil.isEmpty(uuid)) {
-			return findByDN(filter, InternetOrganizationalPerson.USER_ID + CoreConstants.EQ + uuid + "," + organizationalUnit);
+			return findByDN(filter, InternetOrganizationalPerson.USER_ID + CoreConstants.EQ + uuid + CoreConstants.COMMA + organizationalUnit);
 		}
 
 		throw new IllegalArgumentException("UUID is not provided, but required!");
@@ -276,7 +275,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 				getSettings().setProperty(PROPERTY_DOMAIN_DN, dn);
 				getSettings().setProperty(PROPERTY_BASE_DN, dn);
 			}
-			
+
 			SearchResult existingEntities = null;
 			try {
 				existingEntities = findByDN(GroupDAO.GROUP_SEARCH_FILTER, new DN(dn));
@@ -284,7 +283,7 @@ public class ConnectionServiceImpl extends DefaultSpringBean implements Connecti
 
 			if (existingEntities == null) {
 				String timeout = getApplicationProperty(
-						ConnectionService.PROPERTY_RESPONSE_TIMEOUT, 
+						ConnectionService.PROPERTY_RESPONSE_TIMEOUT,
 						ConnectionService.DEFAULT_RESPONSE_TIMEOUT.toString());
 
 				AddRequest request = new AddRequest(new DN(dn));
